632f39bac09bb57572e383b566891bb6f312e62f
Eric Geib <ericgeib@gmail.com>
2011-01-06 16:17:57 -0500

    exercise 24

---
+++ ex24/ex24.py
@@ -0,0 +1,15 @@
+print "Let's practice everything ."
+print 'You\'d need to know \'bout escapes with\\ that do \n newlines and \t tabs.'
+
+poem = """
+\tThe lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an explanation
+\n\t\twhere there is none.
+"""
+
+print "------------"
+print poem
+print "------------"
