2bffc64b51519eebec30521523ef285729a5ea7c
Eric Geib <ericgeib@gmail.com>
2011-12-02 14:25:39 -0500

    ex02 done

---
+++ ex02.py
@@ -0,0 +1,9 @@
+# A coment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print "I could have code lie this." # and the comment after is ignored
+
+# You can also use a comment to to "disable" or comment out a piece of code:
+# print "This won't run."
+
+print "This will run."
